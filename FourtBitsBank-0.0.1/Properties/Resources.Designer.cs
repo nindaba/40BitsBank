2d8b8c1f13acfaad8f3b8d138ec37ac0f39c0720
nindaba <arthurninda@gmail.com>
2021-12-23 19:45:58 +0100

    removed all the pictures successfuly

--- FourtBitsBank-0.0.1/Properties/Resources.Designer.cs
+++ FourtBitsBank-0.0.1/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace FourtBitsBank_0._0._1.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap back {
+            get {
+                object obj = ResourceManager.GetObject("back", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
