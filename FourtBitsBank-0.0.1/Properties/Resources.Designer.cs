98a57ee57be3ec1ebb50c972e28f2f0ac3eefbb7
nindaba <arthurninda@gmail.com>
2021-12-25 01:30:30 +0100

    mist

--- FourtBitsBank-0.0.1/Properties/Resources.Designer.cs
+++ FourtBitsBank-0.0.1/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FourtBitsBank_0._0._1.Properties {
+namespace FourtBitsBank_0.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace FourtBitsBank_0._0._1.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FourtBitsBank_0._0._1.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FourtBitsBank_0.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
